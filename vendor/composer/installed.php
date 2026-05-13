a75fdbcaaff2fd9ebe42c8d4c12e5f393adac99c
fqubp <95485591+fqubp@users.noreply.github.com>
2026-05-13 13:48:00 +0000

    Include vendor directory for deployment

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'fqubp/chisto-pro39',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ed8e6292b505447ba352eaf2610b17285c5df82f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'fqubp/chisto-pro39' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ed8e6292b505447ba352eaf2610b17285c5df82f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.2',
+            'version' => '7.0.2.0',
+            'reference' => 'ebf1655bd5b99b3f97e1a3ec0a69e5f4cd7ea088',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
